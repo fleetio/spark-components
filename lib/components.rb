3bfdb06d5938dbb478081b18b26c74347397bad9
Brandon Mathis <brandon@imathis.com>
2019-04-22 14:48:29 -0500

    Prevent build from failing if no app/components directory

--- lib/components.rb
+++ lib/components.rb
@@ -11,6 +11,8 @@ module Components
   end
 
   def self.component_names
+    return [] unless Dir.exist?(components_path)
+
     Dir.chdir(components_path) do
       Dir.glob("**/*_component.rb").map { |component| component.chomp("_component.rb") }.sort
     end
